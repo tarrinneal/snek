4da6c4bf60c662f21de9a89029f24b47e7a728c8
tarrinneal <tarrinneal@gmail.com>
2021-01-13 14:24:16 -0800

    added breakable bricks

--- public/app.js
+++ public/app.js
@@ -19,6 +19,22 @@ $(document).ready(function() {
   var rightPressed = false;
   var leftPressed = false;
 
+  var brickRowCount = 3;
+  var brickColumnCount = 5;
+  var brickWidth = 75;
+  var brickHeight = 20;
+  var brickPadding = 10;
+  var brickOffsetTop = 50;
+  var brickOffsetLeft = 60;
+  var bricks = [];
+  for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+      bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+  }
+
+
   function colorChange() {
     var colors = ['red', 'blue', 'black', 'grey', 'green', 'yellow', 'brown', 'purple', 'orange'];
     var random = Math.floor(Math.random() * colors.length);
@@ -39,6 +55,24 @@ $(document).ready(function() {
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+  }
+
+  function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+      for(var r=0; r<brickRowCount; r++) {
+        if(bricks[c][r].status == 1) {
+          var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+          var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          ctx.fillStyle = "#0095DD";
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
   }
 
   function draw() {
@@ -77,6 +111,8 @@ $(document).ready(function() {
 
     drawBall();
     drawPaddle();
+    drawBricks();
+    collisionDetection();
   }
 
   function keyUpHandler(e) {
@@ -105,6 +141,20 @@ $(document).ready(function() {
       leftPressed = false;
     }
   }
+
+  function collisionDetection() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
 
   document.addEventListener("keydown", keyDownHandler, false);
   document.addEventListener("keyup", keyUpHandler, false);
